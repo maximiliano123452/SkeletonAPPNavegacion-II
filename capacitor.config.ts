4b3d1334b2a778fc54173c38b3f9a07d6bf10ed6
maxi <maxi.negrete@duocuc.cl>
2025-06-14 22:46:58 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SkeletonAPPNavegacion-II',
+  webDir: 'www'
+};
+
+export default config;
